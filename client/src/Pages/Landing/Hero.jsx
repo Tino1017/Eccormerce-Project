ae6121f0a89401c3a5b77ecc14e88be3fe12d560
Tino1017 <tmagobeya1@gmail.com>
2024-06-09 09:49:41 +0200

    mobile hero added

--- client/src/Pages/Landing/Hero.jsx
+++ client/src/Pages/Landing/Hero.jsx
@@ -8,13 +8,14 @@ import { ShopBanner } from "../../Components/LandingComponents/ShopBanner";
 import { useState,useEffect } from "react";
 import { Audio } from 'react-loader-spinner';
 
+
 export const Hero = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch products from the API
+   
     const fetchProducts = async () => {
       try {
         const response = await fetch('https://fakestoreapi.com/products?limit=5');
--- client/src/Pages/Landing/LandingPage.jsx
+++ client/src/Pages/Landing/LandingPage.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Navigation} from '../../Components/LandingComponents/Navigation'
 import { HashLink as Link } from "react-router-hash-link";
 import { Hero } from "./Hero";
+
+
 const LandingPage = () => {
   return (
     <>
       <Navigation />
       <div className="lg:px-36">
+        
         <Hero />
       </div>
     </>
